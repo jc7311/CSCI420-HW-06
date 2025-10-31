71fecbfa93b31d69d06fe03d8e64807126421814
ch4ngs4n1ty <echang0970@gmail.com>
2025-10-31 02:19:44 -0400

    added euclidean function

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import csv
 import pandas as pd
 import numpy as np
 
+def euclidean_distance(center1, center2):
+
+    return np.sqrt(np.sum((center1- center2) ** 2))
+
+
 def main():
 
     """
@@ -26,19 +31,67 @@ def main():
     #feature_names = df.columns[1:].tolist()
     #print(feature_names)
 
-    print(features)
+    #print(features)
 
     corr_matrix = features.corr().round(2)
-
-
     
     corr_matrix.to_csv('correlated_matrix.csv')
 
-    abs_corr = corr_matrix.abs()
-    np.fill_diagonal(abs_corr.values, 0)
+    """
+    
+    Part B
+    
+    """
 
+    # Start of agglomerative clustering, assign each record to its own cluster protoype
+    # Suppose there are 1000+ records meaning start with 1000+ clusters and 1000+ protoypes of those clusters
 
+    clusters = []
+    
+    n_size = features.shape[0]
 
+    #print(features.values)
+
+    data_points = features.values
+    #print(len(data_points))
+
+    #print(data_points)
+
+    for i in range(len(data_points)):
+
+        clusters.append({
+
+            'center': data_points[i], # prototype - book purchase counts for each row
+            'clusters': [i] # cluster itself, can represent guest id
+
+        })
+
+    cluster_size = len(clusters)
+
+    min_dist = 0.0
+    cluster_i = 0
+    cluster_j = 0
+
+    for i in range(cluster_size):
+        for j in range(i+1, cluster_size):
+
+            dist = euclidean_distance(clusters[i]['center'], clusters[j]['center'])
+
+            if dist < min_dist:
+
+                min_dist = dist
+                cluster_i = i
+                cluster_j = j
+
+    print(dist)
+
+        
+
+    
+
+    #print(clusters)
+
+    #print(clusters)
 
     #print(abs_corr)
 
@@ -49,7 +102,8 @@ def main():
 
     #print(strongly_correlated)
     
-
 if __name__ == "__main__":
 
     main()
+
+
